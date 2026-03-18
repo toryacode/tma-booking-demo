c29d2356cda06182c09e50de6ef3d621a83d0c21
Торя <tarasromanko@MacBook-Pro-Taras.local>
2026-03-18 08:37:55 +0300

    loyalty system

--- backend/app/models/employee_service.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, ForeignKey, Table
-from app.db.base import Base
-
-employee_services = Table(
-    "employee_services",
-    Base.metadata,
-    Column("employee_id", Integer, ForeignKey("employees.id"), primary_key=True),
-    Column("service_id", Integer, ForeignKey("services.id"), primary_key=True),
-)
